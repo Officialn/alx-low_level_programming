f9a4b5d9d802c9e2da08a2160801b8e7515c0a5b
Officialn <nizameshetu@gmail.com>
2022-09-14 20:11:59 +0300

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - function to print abc 10 times
+ * print_alphabet - function to print abc
  *
  * Return: 0
  */
 void print_alphabet_x10(void)
 {
-	char c, i;
+	char c;
 
 	for (i = 0; i <= 9; i++)
 	{
