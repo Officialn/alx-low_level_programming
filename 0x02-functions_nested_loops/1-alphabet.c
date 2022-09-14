8e133f7f211c249a52213c33f5a87c5e08c4638c
Officialn <nizameshetu@gmail.com>
2022-09-14 20:18:21 +0300

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,12 +8,9 @@ void print_alphabet(void)
 {
 	char c;
 
-	for (i = 0; i <= 9; i++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-		}
+		_putchar(c);
+	}
 _putchar('\n');
-	
 }
