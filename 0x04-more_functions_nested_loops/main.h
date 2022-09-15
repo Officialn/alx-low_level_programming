1f58e137b71f4ff65d7b75337c0b2e1f2363f18a
Officialn <nizameshetu@gmail.com>
2022-09-15 15:48:00 +0300

    main.h

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,45 @@
+main.h.txt
+Who has access
+A
+System properties
+Type
+Text
+Size
+456 bytes
+Storage used
+456 bytes
+Location
+More functions, more nested loops
+Owner
+Antony Bahati
+Modified
+1:49 PM by Antony Bahati
+Opened
+3:46 PM by me
+Created
+1:51 PM
+No description
+Viewers can download
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+*main - header file for protoypes
+*void protoypes (void)
+*int protoypes (int)
+*/
+
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+
+#endif /* MAIN_H */
