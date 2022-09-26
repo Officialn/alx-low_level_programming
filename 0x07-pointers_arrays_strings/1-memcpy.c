816e4a12fcb0dc322f803fc64e5b0678d3adc467
Officialn <nizameshetu@gmail.com>
2022-09-26 13:36:31 +0300

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,21 +1,22 @@
-#include "main.h"                                                                                                                                                                                                      
-/**                                                                                                                              
- * _memcpy - Copies @n bytes from the memory area pointed                                                                            
- *           to by @src into that pointed to by @dest.                                                                               
- * @dest: A pointer to the memory area to copy @src into.                                                                            
- * @src: The source buffer to copy characters from.                                                                                  
- * @n: The number of bytes to copy from @src.                                                                                        
+#include "main.h"                                                                                                                                                                                                                  
+/**                                                                                                                                  
+ * _strncat - Concatenates two strings using at most                                                                                 
+ *            an inputted number of bytes from src.                                                                                  
+ * @dest: The string to be appended upon.                                                                                            
+ * @src: The string to be appended to dest.                                                                                          
+ * @n: The number of bytes from src to be appended to dest.                                                                          
  *                                                                                                                                   
- * Return: A pointer to the destination buffer @dest.                                                                                
+ * Return: A pointer to the resulting string dest.                                                                                   
  */                                                                                                                                  
-char *_memcpy(char *dest, char *src, unsigned int n)                                                                                 
+char *_strncat(char *dest, char *src, int n)                                                                                         
 {                                                                                                                                    
-        unsigned int index;                                                                                                          
-        unsigned char *destination = dest;                                                                                           
-        const unsigned char *source = src;                                                                                           
+        int index = 0, dest_len = 0;                                                                                                 
                                                                                                                                      
-        for (index = 0; index < n; index++)                                                                                          
-                destination[index] = source[index];                                                                                  
+        while (dest[index++])                                                                                                        
+                dest_len++;                                                                                                          
+                                                                                                                                     
+        for (index = 0; src[index] && index < n; index++)                                                                            
+                destt[dest_len++] = src[index];                                                                                       
                                                                                                                                      
         return (dest);                                                                                                               
 }
