dab2080bef0bea5bdc62e36ad6892cff0112b1e3
Officialn <nizameshetu@gmail.com>
2022-09-27 08:50:30 +0300

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,22 +1,14 @@
 #include "main.h"                                                                                                                                                                                                                  
-/**                                                                                                                                  
- * _strncat - Concatenates two strings using at most                                                                                 
- *            an inputted number of bytes from src.                                                                                  
- * @dest: The string to be appended upon.                                                                                            
- * @src: The string to be appended to dest.                                                                                          
- * @n: The number of bytes from src to be appended to dest.                                                                          
- *                                                                                                                                   
- * Return: A pointer to the resulting string dest.                                                                                   
- */                                                                                                                                  
-char *_strncat(char *dest, char *src, int n)                                                                                         
+/**                                                                                           * _memcpy - a function that copies memory                                               
+ * @dest:copy to                                        
+ * @src:copy from                                                                            
+ * @n:no of bytes                                                                             * Return: dest                                                                               */                                                                                                                                  
+char *_memcpy(char *dest, char *src,unsigned int n)                                                                                         
 {                                                                                                                                    
-        int index = 0, dest_len = 0;                                                                                                 
+        unsigned  int i;                                                                                                              
                                                                                                                                      
-        while (dest[index++])                                                                                                        
-                dest_len++;                                                                                                          
-                                                                                                                                     
-        for (index = 0; src[index] && index < n; index++)                                                                            
-                destt[dest_len++] = src[index];                                                                                       
+        for (i = 0; i < n; i++)                                                                            
+                dest[i] = src[i];                                                                                       
                                                                                                                                      
         return (dest);                                                                                                               
 }
